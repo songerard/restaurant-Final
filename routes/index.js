d0fe97c75d9b8667e2d35e29370b26ba05ccd8a4
songerard <songerard@gmail.com>
2021-10-20 09:59:37 +0800

    feat: add users module

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,12 @@ const router = express.Router()
 // require home module
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-// const search = require('./modules/search')
+const users = require('./modules/users')
 
 // redirect request
-router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
+router.use('/', home)
 
 
 // export router
